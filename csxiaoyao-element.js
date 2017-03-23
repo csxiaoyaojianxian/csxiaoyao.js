629e636a53cbc0a601532951448033422dc21ac2
csxiaoyaojianxian <1724338257@qq.com>
2017-03-23 11:38:56 +0800

    更新元素操作

--- csxiaoyao-element.js
+++ csxiaoyao-element.js
@@ -2,24 +2,30 @@
 * @Author: SUNSHINE
 * @Date:   2017-03-23 00:05:42
 * @Last Modified by:   SUNSHINE
-* @Last Modified time: 2017-03-23 00:06:04
+* @Last Modified time: 2017-03-23 10:57:36
 */
-
-'use strict';
-
-function show(ele){
-    ele.style.display = "block";
-}
-
-/**
- * 获取元素样式兼容写法
- * @param ele
- * @param attr
- * @returns {*}
- */
-function getStyle(ele,attr){
-    if(window.getComputedStyle){
-        return window.getComputedStyle(ele,null)[attr];
+(function(csxiaoyao){
+	csxiaoyao.show = function(ele){
+	    ele.style.display = "block";
+	}
+	csxiaoyao.hide = function(ele){
+        ele.style.display = "none";
     }
-    return ele.currentStyle[attr];
-}
+	/**
+	 * 获取元素样式兼容写法
+	 */
+	csxiaoyao.getStyle = function(ele,attr){
+	    if(window.getComputedStyle){
+	        return window.getComputedStyle(ele,null)[attr];
+	    }
+	    return ele.currentStyle[attr];
+	}
+	/**
+	 * 获取元素在整个页面的位置
+	 */
+	csxiaoyao.elementSite = function (ele) {
+        var x = ele.offsetLeft;
+        var y = ele.offsetTop;
+        return {x:x,y:y};
+    }
+})(csxiaoyao);
